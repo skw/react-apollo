a763b9474ccf69aca8612f04f2cf677c539c4ffe
James Baxley <james.baxley@newspring.cc>
2016-05-11 20:31:33 -0400

    setup tracking, and failing of build based on file size (#52)

--- scripts/filesize.js
+++ scripts/filesize.js
@@ -30,3 +30,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
