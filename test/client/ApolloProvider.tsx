650e8f1d6ca609b61a88afaf18783ea5a03e0738
Brandon Dail <Aweary@users.noreply.github.com>
2016-07-03 21:41:39 -0500

    Throw an error if the client is missing (#84)
    The current error just states that it can't read initStore
    of undefined, which is a pretty cryptic error if you're not
    familiar with the internals of ApolloProvider.
    
    This just throws a more friendly error.
    
    Linting fix and test case
    
    Use invariant to throw error

--- test/client/ApolloProvider.tsx
+++ test/client/ApolloProvider.tsx
@@ -94,3 +94,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
