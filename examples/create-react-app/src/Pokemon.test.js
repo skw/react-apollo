7c20ca8c61995bddf370c538e47183ff16755dfb
James Baxley <james.baxley@newspring.cc>
2017-05-06 23:03:39 -0400

    A nectarine of the grape is assumed to be a frank squirrel.

--- examples/create-react-app/src/Pokemon.test.js
+++ examples/create-react-app/src/Pokemon.test.js
@@ -128,3 +128,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
--- rollup.config.js
+++ rollup.config.js
@@ -8,3 +8,9 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
