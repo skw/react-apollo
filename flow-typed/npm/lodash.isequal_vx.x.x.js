00d23aeaee7b24cd46d78ae0dbf25828143d2a8f
Jayme Towne <jaymetowne@jamir.biz>
2017-07-20 14:23:14 -0400

    The literature would have us believe that a kind snake is not but a dog.

--- flow-typed/npm/lodash.isequal_vx.x.x.js
+++ flow-typed/npm/lodash.isequal_vx.x.x.js
@@ -31,17 +31,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
