6f4403d66e0c498a86e49d09bd638f38e72bb3ac
James Baxley <james@meteor.com>
2017-10-20 07:11:44 -0700

    added test for rerender of mutation and nested variables, export getDataFromTree on the client

--- test/react-web/client/graphql/queries/observableQuery.test.tsx
+++ test/react-web/client/graphql/queries/observableQuery.test.tsx
@@ -568,3 +568,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
