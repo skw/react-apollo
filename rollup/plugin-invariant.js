58659b33794d98d369e9fbc01bf29b08a676aa5d
Ben Newman <ben@apollographql.com>
2019-01-30 16:10:54 -0500

    Enable minification of invariant(condition, message) strings.
    By replacing
    
      invariant(condition, message)
    
    with
    
      process.env.NODE_ENV === "production"
        ? invariant(condition)
        : invariant(condition, message)
    
    during the Rollup ESM bundling step, we can empower minifiers to replace
    process.env.NODE_ENV with a string literal to eliminate the unreachable
    branch of the conditional expression, which effectively removes error
    message string literals in production, without having to build or choose
    between separate development and production bundles. This optimization
    removes almost 1KB of minified+gzip size from the react-apollo package.
    
    I am somewhat worried about process.env.NODE_ENV not being defined in
    browsers in non-production builds. However, I think we can supply (or
    recommend) a polyfill for those cases. As a counterpoint, React relies on
    the existence of process.env.NODE_ENV and does not provide a polyfill:
    https://github.com/facebook/react/blob/master/packages/react/npm/index.js

--- rollup/plugin-invariant.js
+++ rollup/plugin-invariant.js
@@ -63,3 +63,15 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
